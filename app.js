c23e284f9c7b8f7eb67d5c1a38dd803d93122275
gorkemyagci <gorkemyagci705@gmail.com>
2023-10-21 13:47:18 +0300

    qr-menu-backend

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3001;
 
-mongoose.connect('mongodb://localhost:27017/qr-menu-db', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://gorkemyagci705:35gorkem35@cluster0.get8xcl.mongodb.net/qr-menu-db?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 let allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -21,7 +21,7 @@ let allowCrossDomain = function (req, res, next) {
 };
 
 app.use(allowCrossDomain);
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -32,7 +32,7 @@ app.use(fileUpload({
     tempFileDir: '/tmp/',
 }));
 app.use(session({
-    store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/qr-menu-db' }),
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://gorkemyagci705:35gorkem35@cluster0.get8xcl.mongodb.net/qr-menu-db?retryWrites=true&w=majority' }),
     secret: 'my_secret_key',
     resave: false,
     saveUninitialized: true,
