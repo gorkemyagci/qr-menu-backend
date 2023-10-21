eadc88b25fecda36aad82e01b1fe85e1d1555cfb
gorkemyagci <gorkemyagci705@gmail.com>
2023-10-21 16:00:48 +0300

    backend

--- app.js
+++ app.js
@@ -43,7 +43,11 @@ app.use(session({
 }));
 app.use(allowCrossDomain);
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(fileUpload({
